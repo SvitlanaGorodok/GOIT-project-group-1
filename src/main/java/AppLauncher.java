f0115ac92f5faecfd37d71058ff16be089390187
PavloIv <95400105+PavloIv@users.noreply.github.com>
2022-07-16 12:40:53 +0300

    CreateBotWithStartAndFictitiousButtonSettingsAndGetInfo

--- src/main/java/AppLauncher.java
+++ src/main/java/AppLauncher.java
@@ -1,2 +1,12 @@
-public class AppLauncher {
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class AppLauncher  {
+    public static void main(String[] args) throws TelegramApiException {
+        CurrencyInfoBot currencyInfoBot = new CurrencyInfoBot();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(currencyInfoBot);
+    }
+
 }
---
+++ src/main/java/CurrencyInfoBot.java
@@ -0,0 +1,63 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class CurrencyInfoBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotUsername() {
+        return "@CurrencyInfoProjectGroup1Bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5416117406:AAE1XHQxbn8TIY2perQrAAiQsNcxlcth9Wo";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            try {
+                handleMessage(update.getMessage());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void handleMessage(Message message) throws TelegramApiException {
+        if (message.hasText() && message.hasEntities()) {
+            Optional<MessageEntity> comandEntity = message.getEntities().stream()
+                    .filter(e -> "bot_command".equals(e.getType())).findFirst();
+            if (comandEntity.isPresent()){
+                String commad = message.getText()
+                        .substring(comandEntity.get().getOffset(),comandEntity.get().getLength());
+                switch (commad){
+                    case  "/start":
+                        List<List<InlineKeyboardButton>> buttonsSetingsAndInfo = new ArrayList<>();
+                        buttonsSetingsAndInfo.add(Arrays.asList(InlineKeyboardButton.builder().text("Отримати інфо").
+                                callbackData("GET_INFO").build(),InlineKeyboardButton.builder().text("Налаштування").
+                                callbackData("SETTINGS").build()));
+                        execute(SendMessage.builder()
+                                .text("Ласкаво просимо.Цей бот дозволить відслідкувати актуальні курси валют").
+                                chatId(message.getChatId().toString())
+                                .replyMarkup(InlineKeyboardMarkup.builder()
+                                        .keyboard(buttonsSetingsAndInfo).build())
+                                .build());
+                        return;
+                }
+            }
+        }
+    }
+}
