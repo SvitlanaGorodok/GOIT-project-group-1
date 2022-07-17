5f30fa06782490fd6b139cce05dae7417134157e
MaksBbn <Maksym.Babenko.bbn@gmail.com>
2022-07-17 20:26:23 +0300

    test branch

--- src/test/java/settings/Currency.java
+++ src/test/java/settings/Currency.java
@@ -1,19 +1,44 @@
 package settings;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Currency {
+
+
     USD ("USD", true),
     EUR ("EUR", false),
     PLN ("PLN", false),
     BTC ("BTC", false);
 
+    public static boolean USD_selected = true;
+    public static boolean EUR_selected = true;
+
+
     Currency(String currency, boolean select) {
+
     }
 
 
 
-    public String getSelectedCurrensy() {
-        StringBuilder sb = new StringBuilder();
+    public  static String getSelectedCurrensy() {
+        List selected = new ArrayList<>();
+        if (USD_selected) {
+            selected.add("USD");
+        }
+        if (EUR_selected) {
+            selected.add("EUR");
+        }
 
-        return sb.toString();
+        String result = selected.stream().collect(Collectors.joining(", ", "(", ")")).toString();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSelectedCurrensy());
     }
 }
+
+
