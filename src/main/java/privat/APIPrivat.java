e625389a2ea57ef9bc0a5f3e109d2947eecdafe5
JuliyaShevchuk <ju_shevchuk@ukr.net>
2022-07-17 22:13:23 +0300

    Add getPrivat version 2

--- src/main/java/privat/APIPrivat.java
+++ src/main/java/privat/APIPrivat.java
@@ -1,14 +1,26 @@
 package privat;
 
+import serviceClasses.Bank;
+
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
 public class APIPrivat {
     private static final String PRIVAT_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+    private static final String PRIVAT_PLZ_URL = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=12";
 
     public static void main(String[] args) throws IOException, InterruptedException {
         final List<Private> date = HttpUtil.sendGetBank(URI.create(PRIVAT_URL));
-        System.out.println("Our Private "+date);
+        final List<Private> datePlz = HttpUtil.sendGetBank(URI.create(PRIVAT_PLZ_URL));
+        for (Private currency : datePlz) {
+            if (currency.getCcy().equals("PLZ")) {
+                date.add(currency);
+                break;
+            }
+        }
+        System.out.println("API Privat " + date);
+        final Bank bank = HttpUtil.getPrivat(date);
+        System.out.println(bank.toString());
     }
 }
