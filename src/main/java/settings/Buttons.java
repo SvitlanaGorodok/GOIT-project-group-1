d33c4cd420eeb57396306840bffdd1615040bc95
JuliyaShevchuk <ju_shevchuk@ukr.net>
2022-07-21 20:48:08 +0300

    Add enum Buttons

---
+++ src/main/java/settings/Buttons.java
@@ -0,0 +1,27 @@
+package settings;
+
+public enum Buttons {
+    START ("Старт","/start"),
+    GET_INFO("Отримати інфо","GET_INFO"),
+    SETTINGS("Налаштування","SETTINGS"),
+    NUM_DECIMAL_PLACES ("Кількість знаків після коми","NumDecimalPlaces"),
+    BANK("Банк","Bank"),
+    CURRENCY ("Валюта","Currency"),
+    NOTIFICATION ("Час сповіщення","Notification");
+
+    private String buttonsName;
+    private String buttonsNameEN;
+
+    Buttons(String buttonsName, String buttonsNameEN) {
+        this.buttonsName = buttonsName;
+        this.buttonsNameEN = buttonsNameEN;
+    }
+
+    public String getName() {
+        return buttonsName;
+    }
+
+    public String getNameEN() {
+        return buttonsNameEN;
+    }
+}
