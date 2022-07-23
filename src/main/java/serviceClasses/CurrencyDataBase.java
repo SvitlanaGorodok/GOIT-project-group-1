36ab79b3cb4971b4a36bec93b04076c5e466ef63
Oleksii Bikinieiev <Tirexys@gmail.com>
2022-07-23 23:21:46 +0300

    add synchronized CurrencyDataBase class

--- src/main/java/serviceClasses/CurrencyDataBase.java
+++ src/main/java/serviceClasses/CurrencyDataBase.java
@@ -12,50 +12,52 @@ import java.util.HashMap;
 
 public class CurrencyDataBase {
     public static HashMap<Banks, Bank> currentInfo = new HashMap<>();
+    private static final Object monitor = new Object();
 
     public static Bank getCurrentInfo(Banks bankName) {
-        if (currentInfo.get(bankName) == null) {
-            try {
-                setCurrentInfo(bankName);
-            } catch (InterruptedException | IOException e) {
-                e.printStackTrace();
+        synchronized (monitor) {
+            if (currentInfo.get(bankName) == null) {
+                try {
+                    setCurrentInfo(bankName);
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
             }
+            return currentInfo.get(bankName);
         }
-        return currentInfo.get(bankName);
     }
 
     public static void setCurrentInfo(Banks bankName) throws IOException, InterruptedException {
-        switch (bankName) {
-            case PRIVATE:
-                Bank bankPrivate = APIPrivat.getPrivatAPI();
-                bankPrivate.setTime(LocalDateTime.now());
-                currentInfo.put(bankName, bankPrivate);
-                System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
-                break;
-            case MONO:
-                Bank bankMono = APIMonobank.getMonoAPI();
-                bankMono.setTime(LocalDateTime.now());
-                currentInfo.put(bankName, bankMono);
-                System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
-                break;
-            case NBU:
-                Bank bankNBU = APINbu.getNBUAPI();
-                bankNBU.setTime(LocalDateTime.now());
-                currentInfo.put(bankName, bankNBU);
-                System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
-                break;
+        synchronized (monitor) {
+            switch (bankName) {
+                case PRIVATE:
+                    Bank bankPrivate = APIPrivat.getPrivatAPI();
+                    bankPrivate.setTime(LocalDateTime.now());
+                    currentInfo.put(bankName, bankPrivate);
+                    System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
+                    break;
+                case MONO:
+                    Bank bankMono = APIMonobank.getMonoAPI();
+                    bankMono.setTime(LocalDateTime.now());
+                    currentInfo.put(bankName, bankMono);
+                    System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
+                    break;
+                case NBU:
+                    Bank bankNBU = APINbu.getNBUAPI();
+                    bankNBU.setTime(LocalDateTime.now());
+                    currentInfo.put(bankName, bankNBU);
+                    System.out.println("HashMap " + currentInfo + " розмір " + currentInfo.size());
+                    break;
+            }
         }
-
     }
 
     public Bank getCurrencyRates(Banks bankName) throws IOException, InterruptedException {
-        Bank bank = currentInfo.get(bankName);
-        long timeDiff = Duration.between(LocalDateTime.now(), bank.getTime()).toMinutes();
-        if (timeDiff > 5) {
-            setCurrentInfo(bankName);
-        }
-        return currentInfo.get(bankName);
+            Bank bank = currentInfo.get(bankName);
+            long timeDiff = Duration.between(LocalDateTime.now(), bank.getTime()).toMinutes();
+            if (timeDiff > 5) {
+                setCurrentInfo(bankName);
+            }
+            return currentInfo.get(bankName);
     }
-
-
 }
