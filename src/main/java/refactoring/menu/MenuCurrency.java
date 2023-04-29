4f8f1b4029d77ca85d0a26584a257af512457b7f
svetl <svetlana.akulowa@gmail.com>
2023-04-29 22:56:57 +0300

    refactoring add menus

--- src/main/java/refactoring/menu/MenuCurrency.java
+++ src/main/java/refactoring/menu/MenuCurrency.java
@@ -2,9 +2,14 @@ package refactoring.menu;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
+import java.util.List;
+
 public class MenuCurrency implements Menu{
     @Override
     public SendMessage printMenu(Long chatId) {
-        return null;
+        SendMessage message = new SendMessage(chatId.toString(), "Please choose option:");
+        List<String> buttons = List.of("USD", "EUR", "PLN", "BACK_TO_START", "BACK_TO_SETTINGS");
+        createMenu(buttons, 3, message);
+        return message;
     }
 }
--- src/main/java/refactoring/menu/MenuNotification.java
+++ src/main/java/refactoring/menu/MenuNotification.java
@@ -2,9 +2,16 @@ package refactoring.menu;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
+import java.util.List;
+
 public class MenuNotification implements Menu{
     @Override
     public SendMessage printMenu(Long chatId) {
-        return null;
+        SendMessage message = new SendMessage(chatId.toString(), "Please choose option:");
+        List<String> buttons = List.of("NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN",
+                "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN",
+                "SWICH_OFF", "BACK_TO_START", "BACK_TO_SETTINGS");
+        createMenu(buttons, 5, message);
+        return message;
     }
 }
